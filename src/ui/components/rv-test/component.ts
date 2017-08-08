be1f6027641ed673b2bdc026ad12f9498ea4b168
@anulman <anulman@gmail.com>
2017-08-07 21:49:01 -0400

    feat(rv-test): init component to fill data over websocket

--- src/ui/components/rv-test/component.ts
+++ src/ui/components/rv-test/component.ts
@@ -1,5 +1,19 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from "@glimmer/component";
 
 export default class RvTest extends Component {
+  @tracked data: any = {};
+  private socket: WebSocket = null;
 
+  didInsertElement() {
+    this.socket = new WebSocket('ws://localhost:8080');
+
+    this.socket.addEventListener('message', ({ data }) => {
+      this.data = JSON.parse(data);
+    });
+  }
+
+  willDestroy() {
+    this.socket.close();
+    this.socket = null;
+  }
 }
